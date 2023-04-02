cb4ed0deff28a58d158b5387aef7ec02dd7c1cbd
Nella1a <m.allen@gmx.at>
2023-04-02 14:29:57 +0200

    Add conf for colors, fontsize and fontweight

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-cyan': '#cee3e9',
+        'neon-green': '	#52ffa8',
+        'dark-blue': '#1f2632',
+        'grayish-blue': '#4e5d73',
+        'dark-grayish-blue': '	#323a49',
+      },
+      fontSize: '28px',
+      fontWeight: 800,
+    },
   },
   plugins: [],
 };
