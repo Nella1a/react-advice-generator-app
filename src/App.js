acabf609a2127a2d890a8c54d6d72be51999f523
Nella1a <m.allen@gmx.at>
2023-04-02 12:53:26 +0200

    Refactor quotes component, remove unused images
    - add http request in quotes component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Quotes from './components/Quotes.tsx';
 
 function App() {
-  return <div className="App">x</div>;
+  return (
+    <div className="App">
+      <Quotes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Quotes.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+type APIRESPONSE = {
+  id: number;
+  advice: string;
+};
+
+export default function Quotes() {
+  const [advice, setAdvice] = useState<APIRESPONSE>();
+  useEffect(() => {
+    async function getAdvise() {
+      const response = await fetch('https://api.adviceslip.com/advice', {
+        method: 'GET',
+      });
+      const apiResponse = await response.json();
+      if (apiResponse.status > 200) {
+        console.log('ERRORS');
+      } else {
+        console.log('Response: ', apiResponse);
+        setAdvice(apiResponse.slip);
+      }
+    }
+    getAdvise();
+  }, []);
+  return (
+    <section>
+      <div>
+        <span>ADVICE {advice?.id}</span>
+        <p>{advice?.advice}</p>
+      </div>
+    </section>
+  );
+}
