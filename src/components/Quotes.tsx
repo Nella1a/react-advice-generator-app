ee4b4638fe8871791534c1c62cf0df0a8076a394
Nella1a <m.allen@gmx.at>
2023-04-03 00:03:33 +0200

    Add mobile design, font, and hover effect

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -7,6 +7,7 @@ type APIRESPONSE = {
 
 export default function Quotes() {
   const [advice, setAdvice] = useState<APIRESPONSE>();
+  const [getNewAdvice, setGetNewAdive] = useState(false);
   useEffect(() => {
     async function getAdvise() {
       const response = await fetch('https://api.adviceslip.com/advice', {
@@ -20,32 +21,51 @@ export default function Quotes() {
         setAdvice(apiResponse.slip);
       }
     }
-    getAdvise();
-  }, []);
+    if (getNewAdvice) {
+      getAdvise();
+      setGetNewAdive(false);
+    }
+  }, [getNewAdvice]);
+
+  const onClickHandler = () => {
+    setGetNewAdive(true);
+  };
+
   return (
-    <div
-      className="text-amber-500 w-96 h-64 bg-dark-grayish-blue rounded-lg
-    flex flex-col justify-start items-center p-4 gap-6 relative"
-    >
-      <span className="">ADVICE {advice?.id}</span>
-      <p className="text-quote-size">"{advice?.advice}"</p>
-      <span className="">
+    // <div
+    //   className="text-amber-500 w-7/12 h-80 bg-dark-grayish-blue rounded-lg
+    // flex flex-col justify-start items-center gap-6 relative font-manrope px-2 pt-2 pb-4"
+    // >
+    <div className="h-[30rem] w-11/12  text-amber-500  bg-dark-grayish-blue rounded-lg gap-6 relative font-manrope px-8 pt-8 pb-4  text-center sm:h-96 sm:w-7/12 ">
+      <p className="text-neon-green  tracking-widest text-center">
+        ADVICE #{advice?.id}
+      </p>
+      <p className="h-72 flex justify-center items-center  text-quote-size text-light-cyan m-auto font-extrabold my-6 sm:h-48 sm:min-h-0 ">
+        <span> &#8220;{advice?.advice}&#8221;</span>
+      </p>
+      <div className="flex justify-center">
         <img
           src={'/images/pattern-divider-desktop.svg'}
           alt={'pattern divider'}
           width={'444'}
           height={'16'}
         />
-      </span>
-      <span className="absolute z-10 bottom-0 inset-x-0 bg-neon-green rounded-full w-10 h-10 margin-auto">
-        {' '}
-        <img
-          src={'/images/icon-dice.svg'}
-          alt={'icon dice'}
-          width={'24'}
-          height={'24'}
-        />
-      </span>
+
+        <button
+          className="absolute z-10 -bottom-10 left-50% bg-neon-green
+          hover:shadow-3xl hover:bg-neon-green
+          rounded-full w-20 h-20 margin-auto flex justify-center items-center sm:w-14 sm:h-14 sm:-bottom-7 "
+          onClick={onClickHandler}
+        >
+          {' '}
+          <img
+            src={'/images/icon-dice.svg'}
+            alt={'icon dice'}
+            width={'24'}
+            height={'24'}
+          />
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -10,8 +10,17 @@ module.exports = {
         'grayish-blue': '#4e5d73',
         'dark-grayish-blue': '	#323a49',
       },
+      boxShadow: {
+        /*         '3xl': '0 35px 60px 5px rgba(179, 225, 217, 0.5)',
+         */
+        '3xl': '0 0px 30px 5px rgba(82, 225, 168, 0.5)',
+      },
+
       fontSize: { 'quote-size': '28px' },
       fontWeight: 800,
+      fontFamily: {
+        manrope: ['manrope', 'system-ui'],
+      },
     },
   },
   plugins: [],
