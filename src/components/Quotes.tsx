8428c6afd19ef5d6f91487d8e0622baa39c6ee9b
Nella1a <m.allen@gmx.at>
2023-04-03 22:47:23 +0200

    Refactor font weight, add h1

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -34,9 +34,9 @@ export default function Quotes() {
 
   return (
     <div className="h-[30rem] w-11/12  text-amber-500  bg-dark-grayish-blue rounded-3xl gap-6 relative font-manrope px-8 pt-8 pb-4  text-center sm:h-80 sm:w-[35rem] ">
-      <p className="text-neon-green  tracking-widest text-center font-bold">
+      <h1 className="text-neon-green  tracking-[0.2rem] text-center font-semibold">
         ADVICE #{advice?.id}
-      </p>
+      </h1>
       <p className="h-64 flex justify-center items-center  text-quote-size text-light-cyan m-auto font-extrabold my-6 sm:h-32 sm:min-h-0 ">
         {!errors ? (
           <span> &#8220;{advice?.advice}&#8221;</span>
