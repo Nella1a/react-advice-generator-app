bf0b43be33dced0982ae1db3f683238c30c7b2db
Nella1a <m.allen@gmx.at>
2023-04-03 21:33:47 +0200

    Add more responsive styling

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -36,11 +36,11 @@ export default function Quotes() {
     //   className="text-amber-500 w-7/12 h-80 bg-dark-grayish-blue rounded-lg
     // flex flex-col justify-start items-center gap-6 relative font-manrope px-2 pt-2 pb-4"
     // >
-    <div className="h-[30rem] w-11/12  text-amber-500  bg-dark-grayish-blue rounded-lg gap-6 relative font-manrope px-8 pt-8 pb-4  text-center sm:h-96 sm:w-7/12 ">
-      <p className="text-neon-green  tracking-widest text-center">
+    <div className="h-[30rem] w-11/12  text-amber-500  bg-dark-grayish-blue rounded-lg gap-6 relative font-manrope px-8 pt-8 pb-4  text-center sm:h-96 sm:w-[35rem] ">
+      <p className="text-neon-green  tracking-widest text-center font-bold">
         ADVICE #{advice?.id}
       </p>
-      <p className="h-72 flex justify-center items-center  text-quote-size text-light-cyan m-auto font-extrabold my-6 sm:h-48 sm:min-h-0 ">
+      <p className="h-64 flex justify-center items-center  text-quote-size text-light-cyan m-auto font-extrabold my-6 sm:h-48 sm:min-h-0 ">
         <span> &#8220;{advice?.advice}&#8221;</span>
       </p>
       <div className="flex justify-center">
@@ -54,7 +54,7 @@ export default function Quotes() {
         <button
           className="absolute z-10 -bottom-10 left-50% bg-neon-green
           hover:shadow-3xl hover:bg-neon-green
-          rounded-full w-20 h-20 margin-auto flex justify-center items-center sm:w-14 sm:h-14 sm:-bottom-7 "
+          rounded-full w-20 h-20 margin-auto flex justify-center items-center sm:w-14 sm:h-14 sm:-bottom-7"
           onClick={onClickHandler}
         >
           {' '}
