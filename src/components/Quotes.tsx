80bd1e6eae7799ca29f76b4cd439e4e65108ed98
Nella1a <m.allen@gmx.at>
2023-04-03 22:34:55 +0200

    Refactor container width

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -7,7 +7,7 @@ type APIRESPONSE = {
 
 export default function Quotes() {
   const [advice, setAdvice] = useState<APIRESPONSE>();
-  const [getNewAdvice, setGetNewAdive] = useState(false);
+  const [getNewAdvice, setGetNewAdive] = useState(true);
   const [errors, setErrors] = useState<string | undefined>();
   useEffect(() => {
     async function getAdvise() {
@@ -21,6 +21,7 @@ export default function Quotes() {
         setAdvice(apiResponse.slip);
       }
     }
+
     if (getNewAdvice) {
       getAdvise();
       setGetNewAdive(false);
@@ -32,11 +33,11 @@ export default function Quotes() {
   };
 
   return (
-    <div className="h-[30rem] w-11/12  text-amber-500  bg-dark-grayish-blue rounded-lg gap-6 relative font-manrope px-8 pt-8 pb-4  text-center sm:h-96 sm:w-[35rem] ">
+    <div className="h-[30rem] w-11/12  text-amber-500  bg-dark-grayish-blue rounded-3xl gap-6 relative font-manrope px-8 pt-8 pb-4  text-center sm:h-80 sm:w-[35rem] ">
       <p className="text-neon-green  tracking-widest text-center font-bold">
         ADVICE #{advice?.id}
       </p>
-      <p className="h-64 flex justify-center items-center  text-quote-size text-light-cyan m-auto font-extrabold my-6 sm:h-48 sm:min-h-0 ">
+      <p className="h-64 flex justify-center items-center  text-quote-size text-light-cyan m-auto font-extrabold my-6 sm:h-32 sm:min-h-0 ">
         {!errors ? (
           <span> &#8220;{advice?.advice}&#8221;</span>
         ) : (
