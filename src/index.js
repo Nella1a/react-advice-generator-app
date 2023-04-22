ca29cddab5abc7fd3cfd0024000a90d4bc342bc5
Nella1a <m.allen@gmx.at>
2023-04-23 01:26:37 +0200

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -5,19 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: 'getAdvice',
-//     element: <GetAdvice />,
-//     errorElement: <ErrorPage />,
-//   },
-// ]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
