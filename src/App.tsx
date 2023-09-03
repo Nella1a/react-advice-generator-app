62c0871da190e79fab6c63a6f218c0043efe0e65
Nella1a <m.allen@gmx.at>
2023-09-03 17:30:27 +0200

    Cleanup - remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { FetcherWithComponents, Route, Routes } from 'react-router-dom';
 import RandomAdvice from './components/RandomAdvice';
 import SearchAdvice from './components/SearchAdvice';
 import ErrorPage from './errorPage';
 import Home from './home';
 import Sidebar from './routes/sidebar';
 
-class App extends Component {
-  render() {
-    return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="advice" element={<Sidebar />}>
-          <Route path="get-advice" element={<RandomAdvice />} />
-          <Route path="search-advice" element={<SearchAdvice />} />
-          {/* <Route path="search-advice/results/:id" element={<RandomAdvice />} /> */}
-          <Route path="*" element={<ErrorPage />} />{' '}
-        </Route>
-        <Route path="*" element={<ErrorPage />} />{' '}
-      </Routes>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="advice" element={<Sidebar />}>
+        <Route path="get-advice" element={<RandomAdvice />} />
+        <Route path="search-advice" element={<SearchAdvice />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+};
 
 export default App;
