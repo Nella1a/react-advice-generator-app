60aa216df3c6e4a8609d907e2d77b1dc4cfdc1e6
Nella1a <m.allen@gmx.at>
2023-09-03 17:29:36 +0200

    Refactor component into a functional component, add logic to display link buttons based on current url path

--- src/routes/sidebar.tsx
+++ src/routes/sidebar.tsx
@@ -1,35 +1,46 @@
-import React, { Component } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, Outlet, useHref } from 'react-router-dom';
 import Button from '../components/Button';
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <div className="flex justify-center items-center min-h-screen bg-dark-blue text-light-cyan w-full">
-        <section className="w-1/5 min-h-screen flex flex-col justify-center items-center gap-6">
-          <div>
-            <Link to="/">
-              <Button text={'home'} />
-            </Link>
-          </div>
+const Sidebar = () => {
+  const [currentUrl, setCurrentUrl] = useState('');
+  const urlPath = useHref({});
+
+  useEffect(() => {
+    const getUrlEndpoint = urlPath.split('/');
+    const urlEndpoint = getUrlEndpoint[getUrlEndpoint.length - 1];
+    setCurrentUrl(urlEndpoint);
+  }, [urlPath]);
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-dark-blue text-light-cyan w-full">
+      <section className="w-1/5 min-h-screen flex flex-col justify-center items-center gap-6">
+        <div>
+          <Link to="/">
+            <Button text={'home'} />
+          </Link>
+        </div>
+
+        {currentUrl === 'search-advice' ? (
           <div>
             <Link to="get-advice">
               <Button text={'random advice'} />
             </Link>
           </div>
+        ) : (
           <div>
             <Link to="search-advice">
               <Button text={'search advice'} />
             </Link>
           </div>
-        </section>
-        <section className="w-4/5">
-          {' '}
-          <Outlet />
-        </section>
-      </div>
-    );
-  }
-}
+        )}
+      </section>
+      <section className="w-4/5">
+        {' '}
+        <Outlet />
+      </section>
+    </div>
+  );
+};
 
 export default Sidebar;
