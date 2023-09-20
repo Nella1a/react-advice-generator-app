97a53ab18548b83eff97d6af6f72e29acf112fc9
Nella1a <m.allen@gmx.at>
2023-09-20 23:18:19 +0200

    Setup test

---
+++ src/components/SearchAdvice/index.test.tsx
@@ -0,0 +1,77 @@
+/**
+ * @jest-environment jsdom
+ * @jest-environment-options {"url": "https://localhost"}
+ */
+
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import SearchAdvice from './';
+
+const server = setupServer(
+  rest.get('https://api.adviceslip.com/advice/search/love', (req, res, ctx) => {
+    console.log('REQ: ', req);
+    return res(
+      ctx.status(200),
+      ctx.json({
+        total_results: '3',
+        query: 'love',
+        slips: [
+          {
+            id: 101,
+            advice: 'Always do anything for love.',
+            date: '2015-12-08',
+          },
+          { id: 174, advice: 'Be a good lover.', date: '2014-06-03' },
+          {
+            id: 184,
+            advice: 'Take a chance on doing what you love.',
+            date: '2017-03-10',
+          },
+        ],
+      }),
+    );
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('Search Advice', async () => {
+  render(
+    <BrowserRouter>
+      <SearchAdvice />
+    </BrowserRouter>,
+  );
+
+  //heading
+  expect(
+    screen.getByRole('heading', { name: 'Search Advice' }),
+  ).toBeInTheDocument();
+
+  // input field
+  const inputField = screen.getByRole('searchbox', { name: 'Search Advice' });
+  expect(inputField).toBeInTheDocument();
+  expect(inputField).toHaveValue('');
+
+  // search button
+  expect(screen.getByRole('button', { name: 'Search' })).toBeInTheDocument();
+
+  // add searchTerm
+  await userEvent.type(inputField, 'love');
+  expect(inputField).toHaveValue('love');
+
+  // submit
+  await userEvent.click(screen.getByRole('button', { name: 'Search' }));
+  expect(
+    await screen.findByText('Always do anything for love.'),
+  ).toBeInTheDocument();
+
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.debug();
+});
