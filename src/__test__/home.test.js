a3fc0d65ef5976b493774bb614290ae2d617f2dd
Nella1a <m.allen@gmx.at>
2023-09-20 22:18:20 +0200

    Cleanup

--- src/__test__/home.test.js
+++ src/__test__/home.test.js
@@ -5,7 +5,6 @@
 
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Home from '../home.tsx';
@@ -21,16 +20,5 @@ test('two buttons on screen', async () => {
   ).toBeInTheDocument();
   expect(
     screen.getByRole('button', { name: 'Search Advice' }),
-  ).toBeInTheDocument();
-});
-
-test('link "Get Random Advice" works without errors', async () => {
-  render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>,
-  );
-  expect(
-    screen.getByRole('button', { name: 'Get Random Advice' }),
   ).toBeInTheDocument();
 });
