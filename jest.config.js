e27b4c561e7fcc69d58caa033a10ea5ff51d113b
Nella1a <m.allen@gmx.at>
2023-09-20 19:37:25 +0200

    Refactor jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+const config = {
+  verbose: true,
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.{js,jsx,tsx}'],
+  coveragePathIgnorePatterns: ['src/types/'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  preset: 'ts-jest',
 };
+
+module.exports = config;
