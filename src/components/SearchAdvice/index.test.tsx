e0cc862aa99c834a81c322c1accd7d28920b643d
Nella1a <m.allen@gmx.at>
2023-09-20 23:30:33 +0200

    Add 'whatwg-fetch' as the fetch polyfill to prevent tests from failing due to a 'ReferenceError: fetch is not defined'

--- src/components/SearchAdvice/index.test.tsx
+++ src/components/SearchAdvice/index.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  * @jest-environment-options {"url": "https://localhost"}
  */
-
+import 'whatwg-fetch';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
