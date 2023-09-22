0f5639dc3925c9a8b2b2cf158b40cf404dc2a05b
Nella1a <m.allen@gmx.at>
2023-09-22 21:45:34 +0200

    Add an optional onClick event as a prop.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 type Props = {
   text: string;
+  onClickHandler?: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => void;
 };
 
-const Button = ({ text }: Props) => {
+const Button = ({ text, onClickHandler }: Props) => {
   return (
     <>
       <button
         className={
-          'w-32 h-12  text-center text-dark-blue font-manrope bg-neon-green font-bold hover:text-pink-600 rounded-lg'
+          'w-32 h-12  text-center text-dark-blue font-manrope bg-neon-green font-bold hover:bg-green-100 rounded-lg'
         }
+        onClick={onClickHandler}
       >
         {text}
       </button>
