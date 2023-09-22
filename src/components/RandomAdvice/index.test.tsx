55d8d4c7ab30477b8a4af3d05052ab8fdf43ab94
Nella1a <m.allen@gmx.at>
2023-09-22 14:21:29 +0200

    Add a test to ensure the rendering of the RandomAdviceComponent without errors

---
+++ src/components/RandomAdvice/index.test.tsx
@@ -0,0 +1,52 @@
+/**
+ * @jest-environment jsdom
+ * @jest-environment-options {"url": "https://localhost"}
+ */
+import 'whatwg-fetch';
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import RandomAdvice from './';
+
+const handlers = [
+  rest.get('https://api.adviceslip.com/advice', (req, res, ctx) => {
+    console.log('HELLO');
+    console.log('req: ', req);
+    return res(
+      ctx.body(
+        JSON.stringify({
+          slip: {
+            id: 224,
+            advice: 'Mocked random advice.',
+          },
+        }),
+      ),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('renders the RandomAdvice component', async () => {
+  // mock window alert
+  global.alert = jest.fn();
+  render(
+    <BrowserRouter>
+      <RandomAdvice />
+    </BrowserRouter>,
+  );
+
+  expect(await screen.findByText('ADVICE #224')).toBeInTheDocument();
+  expect(await screen.findByText(/Mocked random advice./)).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'icon dice' })).toBeInTheDocument();
+
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.debug();
+});
