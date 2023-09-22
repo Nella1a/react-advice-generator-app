c8abdb28099652b163765917788bf10feb18a7b9
Nella1a <m.allen@gmx.at>
2023-09-22 20:44:25 +0200

    Add a test case for when there is no match for the searchTerm.

--- src/components/SearchAdvice/index.test.tsx
+++ src/components/SearchAdvice/index.test.tsx
@@ -15,24 +15,25 @@ import SearchAdvice from './';
 const handlers = [
   rest.get('https://api.adviceslip.com/advice/search/love', (req, res, ctx) => {
     return res(
-      ctx.status(200),
-      ctx.json({
-        total_results: '3',
-        query: 'love',
-        slips: [
-          {
-            id: 101,
-            advice: 'Always do anything for love.',
-            date: '2015-12-08',
-          },
-          { id: 174, advice: 'Be a good lover.', date: '2014-06-03' },
-          {
-            id: 184,
-            advice: 'Take a chance on doing what you love.',
-            date: '2017-03-10',
-          },
-        ],
-      }),
+      ctx.body(
+        JSON.stringify({
+          total_results: '3',
+          query: 'love',
+          slips: [
+            {
+              id: 101,
+              advice: 'Always do anything for love.',
+              date: '2015-12-08',
+            },
+            { id: 174, advice: 'Be a good lover.', date: '2014-06-03' },
+            {
+              id: 184,
+              advice: 'Take a chance on doing what you love.',
+              date: '2017-03-10',
+            },
+          ],
+        }),
+      ),
     );
   }),
 ];
@@ -42,7 +43,7 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('Search Advice', async () => {
+test('render SearchAdvice component and  search for an advice', async () => {
   render(
     <BrowserRouter>
       <SearchAdvice />
@@ -59,7 +60,7 @@ test('Search Advice', async () => {
   expect(inputField).toBeInTheDocument();
   expect(inputField).toHaveValue('');
 
-  // search button
+  // submit button
   expect(screen.getByRole('button', { name: 'Search' })).toBeInTheDocument();
 
   // add searchTerm
@@ -71,4 +72,48 @@ test('Search Advice', async () => {
   expect(
     await screen.findByText(/Always do anything for love/),
   ).toBeInTheDocument();
+});
+
+test('No match for SearchTerm', async () => {
+  server.use(
+    rest.get(
+      'https://api.adviceslip.com/advice/search/funnnn',
+      (req, res, ctx) => {
+        return res(
+          ctx.body(
+            JSON.stringify({
+              message: { type: 'notice', text: 'no match found' },
+            }),
+          ),
+        );
+      },
+    ),
+  );
+
+  render(
+    <BrowserRouter>
+      <SearchAdvice />
+    </BrowserRouter>,
+  );
+
+  //heading
+  expect(
+    screen.getByRole('heading', { name: 'Search Advice' }),
+  ).toBeInTheDocument();
+
+  // input field
+  const inputField = screen.getByRole('searchbox', { name: 'Search Advice' });
+  expect(inputField).toBeInTheDocument();
+  expect(inputField).toHaveValue('');
+
+  // submit button
+  expect(screen.getByRole('button', { name: 'Search' })).toBeInTheDocument();
+
+  // add searchTerm
+  await userEvent.type(inputField, 'funnnn');
+  expect(inputField).toHaveValue('funnnn');
+
+  // submit
+  await userEvent.click(screen.getByRole('button', { name: 'Search' }));
+  expect(await screen.findByText(/no match found/)).toBeInTheDocument();
 });
