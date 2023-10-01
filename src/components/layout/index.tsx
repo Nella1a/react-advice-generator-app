5ef612888fa73c240bec46c86d2b8c5d94f2d157
Nella1a <m.allen@gmx.at>
2023-10-01 20:54:26 +0200

    Remove comments

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,9 +6,6 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    // <main className="flex mt-8 md:mt-0 md:justify-center flex-col md:flex-row items-center md:gap-8 min-h-screen bg-dark-blue text-light-cyan ">
-    //   {children}
-    // </main>
     <main className="flex md:mt-0 md:justify-center flex-col md:flex-row items-center md:gap-8 min-h-screen bg-dark-blue text-light-cyan">
       {children}
     </main>
