e5fec1dc9c46b52d3da3f5e801cb473d596fc89a
Nella1a <m.allen@gmx.at>
2025-07-20 22:36:50 +0200

    Add loading state

--- src/components/SearchAdvice/index.tsx
+++ src/components/SearchAdvice/index.tsx
@@ -17,15 +17,17 @@ const SearchAdvice = () => {
   const [search, setSearch] = useState(false);
   const [advice, setAdvice] = useState<SearchAdviceType>(undefined);
   const [error, setError] = useState<ApiError>(undefined);
+  const [loading, setLoading] = useState(false);
 
   const onClickHandlerSearch = (event: any) => {
     if (searchTerm) {
       setSearch(true);
+      setLoading(true);
     }
     setAdvice(undefined);
   };
 
-  const onChangeHandler = (event: {
+  const HandleOnChange = (event: {
     target: { value: React.SetStateAction<string> };
   }) => {
     setSearchTerm(event.target.value);
@@ -38,6 +40,7 @@ const SearchAdvice = () => {
       fetch(`https://api.adviceslip.com/advice/search/${searchTerm}`)
         .then((response) => response.json())
         .then((result) => {
+          setLoading(false);
           if ('slips' in result) {
             setAdvice(result);
           } else {
@@ -60,12 +63,16 @@ const SearchAdvice = () => {
           <SearchTerm
             searchTerm={searchTerm}
             onClickHandlerSearch={onClickHandlerSearch}
-            onChangeHandler={onChangeHandler}
+            onChangeHandler={HandleOnChange}
           />
           {error ? (
             <h2 className="font-semibold w-full text-center mt-10 text-xl">
               {error?.text}
             </h2>
+          ) : loading ? (
+            <p className="font-semibold w-full text-center mt-10 text-xl">
+              Loading...
+            </p>
           ) : (
             advice &&
             advice.slips?.length > 0 && <SearchResults result={advice} />
